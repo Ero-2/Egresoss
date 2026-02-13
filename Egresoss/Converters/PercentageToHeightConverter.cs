c7a1aaf190c5580d8b99b54ce0a7de0217a66a45
Erick Garcia <erickbmxstreetmva@gmail.com>
2026-02-13 11:10:18 -0600

    cambios

--- Egresoss/Converters/PercentageToHeightConverter.cs
+++ Egresoss/Converters/PercentageToHeightConverter.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Globalization;
 
-namespace Egresoss.Converters
+namespace Egresoss.Converters;
+
+public class PercentageToHeightConverter : IValueConverter
 {
-    class PercentageToHeightConverter
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
+        if (value is double percentage)
+            return percentage * 110; // altura máxima 110dp
+        return 0;
     }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
 }
